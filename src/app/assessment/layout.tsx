97e6456ff6ecd4d1133b52740b593b69087b5dc6
Jackson Miller <jackson@Jacksons-MacBook-Pro.local>
2024-11-24 16:51:57 -0500

    init

---
+++ src/app/assessment/layout.tsx
@@ -0,0 +1,3 @@
+// Add whatever we did at TBOL to set the layout here
+
+// Put the instructions in another file in an object and have the getStaticProps pull from them
---
+++ src/zz_testUtils/instructions.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const instructions = {
+  test1: <div>Test 1</div>,
+};
